2896cdce280612ea18fcefdb31c93d1f5456b5bb
Zyper <mehmetarifsen3@gmail.com>
2026-02-07 17:34:29 +0300

    Update script URL in Nortexxx.lua

--- Nortexxx.lua
+++ Nortexxx.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://gist.githubusercontent.com/nortex585/1addf7c662eb4afe50038ecc57ed2d76/raw/453b1a756fddc6461af38319786a004e983b47bb/nortexx.lua"))()
+loadstring(game:HttpGet("https://gist.githubusercontent.com/nortex585/66c6844bbd409b657bb9ff45d26c24f1/raw/33022b8a92f25a1a01290e4e78d5d8bde53baf8e/nortex.lua"))()
